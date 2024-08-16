aeed05920ad01da5f38844d54bc073513ad11e78
ipavlakos <pavlakosIlias@outlook.com>
2024-08-17 00:22:10 +0300

    Going Zoneless! Removing zone.js and only using signals

--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,21 +1,15 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs'
+import {Injectable, signal} from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
 
-  messages$ = new BehaviorSubject<string[]>([]);
+  private messages = signal<string[]>([]);
 
-  private messages : string[] = [];
-
-  get allMessages(){
-    return [...this.messages]
-  }
+  allMessages = this.messages.asReadonly();
 
   addMessage(message: string) {
-    this.messages = [...this.messages, message];
-    this.messages$.next([...this.messages]);
+    this.messages.update((messages) => [...messages, message]);
   }
 }
